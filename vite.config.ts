7efefe67067cb05e9dff2a38ec2b93937bf7a20e
isama78 <isa21005@byui.edu>
2026-06-27 20:11:56 -0300

    fix: explicit conditional base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/frontend-pc-repair-management/"
-      : "/",
-  test: {
-    globals: true, // Permite usar describe, test y expect sin importarlos en cada archivo
-    environment: "jsdom", // Simula el DOM de un navegador
-    setupFiles: "./src/setupTests.ts", // Archivo de configuración inicial para los matchers extendidos
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // Si el comando es 'build', inyecta la ruta de GitHub Pages; si es 'dev', usa la raíz '/'
+    base: command === 'build' ? '/frontend-pc-repair-management/' : '/',
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+    },
+  };
 });
