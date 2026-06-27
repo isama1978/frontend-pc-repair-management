c4e8cc67c4ccfb56a7f28ede46caf9fd5016f546
isama78 <isa21005@byui.edu>
2026-06-27 20:38:35 -0300

    configuring .env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_API_URL: string;
-  // Agregá acá más variables si las necesitás en el futuro
+  readonly VITE_API_BASE_URL: string;
 }
 
 interface ImportMeta {
